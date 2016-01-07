64ab0e19600bbf880445ccdc6495c2c25f28a766
Shajed <shajedul.hoque@rootnext.com>
2016-01-07 19:34:41 +0600

    react-redux-test_project setup working

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+/**
+ * Created by root on 1/7/16.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*var TestApp = React.createClass({
+  render: function(){
+    return <div>
+      <h1>Hello, world! I am a novie Node learner.</h1>
+    </div>;
+  }
+});
+ReactDOM.render(
+  <TestApp/>,
+  document.getElementById('example')
+);*/
+
+var HW = React.createClass({
+  render: ()=>{
+    return <h1>Hello, React and Redux World!</h1>
+  }
+});
+
+ReactDOM.render(
+  <HW/>,
+  document.getElementById("example")
+);
+
+/*ReactDOM.render(
+  //<h1>Hello, World!</h1>,
+  React.createElement('h1', null, 'Hello, React!'),
+  document.getElementById('example')
+);*/
