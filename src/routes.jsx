6d7e85b16af8bb5553eb0936fe825d1be3f33354
mshRoR <shajed_msh@yaooh.com>
2016-01-09 01:15:56 +0600

    route path update

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,7 +8,7 @@ import App from './Components/App.jsx';
 import Dashboard from './Components/Dashboard.jsx';
 import Profile from './Components/Profile.jsx';
 
-const routes = <Route component={App}>
+const routes = <Route path="/" component={App}>
   <Route path="/dashboard" component={Dashboard}/>
   <Route path="/profile" component={Profile}/>
 </Route>;
